3466b5147aed8c725f267c1a66f29283c02337c6
Ritesh Singh <1124ritesh@gmail.com>
2022-10-02 17:23:43 +0530

    transactions in progress
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ db/sqlc/store.go
@@ -0,0 +1,16 @@
+package db
+
+import "database/sql"
+
+// Store Provides all functions to execute all queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
